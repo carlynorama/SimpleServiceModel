953311a8fd3c7f734cc5490e95208e5845ec9801
carlynorama <carlynorama@gmail.com>
2022-09-02 15:53:32 -0700

    Initial Commit

---
+++ SimpleServiceModel/SimpleServiceModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleServiceModelApp.swift
+//  SimpleServiceModel
+//
+//  Created by Labtanza on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleServiceModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
